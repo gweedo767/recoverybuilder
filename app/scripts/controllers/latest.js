befee8f2a4c52d94c7baa60ad2671f285696a063
Nathan Grebowiec <njgreb@gmail.com>
2014-09-25 14:26:48 -0500

    show message when no recovery found

--- app/scripts/controllers/latest.js
+++ app/scripts/controllers/latest.js
@@ -8,7 +8,7 @@
  * Controller of the recoverybuilderApp
  */
 angular.module('recoverybuilderApp')
-  .controller('LatestCtrl', function ($scope, $routeParams, devicefileservice) {
+  .controller('LatestCtrl', function ($scope, $log, $routeParams, devicefileservice) {
     //load current items database
     $scope.deviceName = $routeParams.deviceName;
     $scope.recoveryType = $routeParams.recoveryType;
@@ -16,7 +16,9 @@ angular.module('recoverybuilderApp')
     devicefileservice.async().then(function(data) {
     	var fileList = [];
     	
-    	fileList.push( data[$scope.deviceName][$scope.recoveryType].latest );
+    	if( data[$scope.deviceName][$scope.recoveryType].latest !== undefined) {
+            fileList.push( data[$scope.deviceName][$scope.recoveryType].latest );
+        }
 
 	    $scope.recoveryFiles = fileList;
     });
